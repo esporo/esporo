89b6c962b0304ce15dbb7557c89272a9dfb53b1f
Arthur Freitas <email@arthr.me>
2025-06-03 10:19:38 -0300

    updates stats endpoint

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -2,17 +2,19 @@ function formatNumber(number) {
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 }
 
-let stats = fetch('//universal-cors-proxy.glitch.me/http%3A%2F%2Fwww.spore.com%2Frest%2Fstats')
-.then(response => response.text())
-.then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
-.then((data) => {  
-  let totalUploads = formatNumber(data.querySelector('totalUploads').textContent),
-      totalUsers = formatNumber(data.querySelector('totalUsers').textContent),
-      dailyUploads = formatNumber(data.querySelector('dayUploads').textContent),
-      dailyUsers = formatNumber(data.querySelector('dayUsers').textContent);
-  
-  document.querySelector('[data-total-uploads]').textContent = totalUploads;
-  document.querySelector('[data-total-users]').textContent = totalUsers;
-  document.querySelector('[data-daily-uploads]').textContent = dailyUploads;
-  document.querySelector('[data-daily-users]').textContent = dailyUsers;
-});
+fetch('//spore-o-meter.sporecommunity.com')
+  .then(response => response.json())
+  .then(data => {
+    let totalUploads = formatNumber(data.totalAssetCount),
+        totalUsers = formatNumber(data.totalUsers),
+        dailyUploads = formatNumber(data.dayAssetCount),
+        dailyUsers = formatNumber(data.dayUsers);
+
+    document.querySelector('[data-total-uploads]').textContent = totalUploads;
+    document.querySelector('[data-total-users]').textContent = totalUsers;
+    document.querySelector('[data-daily-uploads]').textContent = dailyUploads;
+    document.querySelector('[data-daily-users]').textContent = dailyUsers;
+  })
+  .catch(error => {
+    console.error('Erro ao buscar os dados:', error);
+  });
